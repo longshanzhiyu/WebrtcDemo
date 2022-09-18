914c6983f726b2e690c4d77b32ec4b92e6efcdfd
shawn·black <longshanzhiyu_2018@163.com>
2022-09-18 12:58:17 +0800

    feature(master):添加反馈header

--- source/server.js
+++ source/server.js
@@ -9,6 +9,6 @@ var options = {
 }
 
 var app = https.createServer(options, function(req, res) {
-    res.writeHead();
+    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('HTTPS: Hello World!\n');
 }).listen(443, '0.0.0.0');
