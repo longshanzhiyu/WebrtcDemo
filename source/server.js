32cf1b26217f37e638784a6cc8d9d425831c7cd2
shawn·black <longshanzhiyu_2018@163.com>
2022-09-19 15:48:17 +0800

    Revert "feature(local):修改证书路径"
    This reverts commit c94749256e7ea8e51a1d7e8e63d1978cdd2626d9.

--- source/server.js
+++ source/server.js
@@ -16,8 +16,8 @@ var http_server = http.createServer(app);
 http_server.listen(80, '0.0.0.0');
 
 var options = {
-    key:    fs.readFileSync('../cert/fbird.xyz.key'),
-    cert:   fs.readFileSync('../cert/fbird.xyz.crt')
+    key:    fs.readFileSync('../cert/key.pem'),
+    cert:   fs.readFileSync('../cert/cert.pem')
 }
 
 //https server
