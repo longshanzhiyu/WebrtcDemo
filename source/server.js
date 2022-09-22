2eccbca568c3a444561009dbfd9101f1784735c2
shawn·black <longshanzhiyu_2018@163.com>
2022-09-22 10:43:20 +0800

    添加信令服务器 添加client样式

--- source/server.js
+++ source/server.js
@@ -4,6 +4,9 @@ var http = require('http');
 var https = require('https');
 var fs = require('fs');
 
+//socket.io
+var socketIo = require('socket.io');
+
 var express = require('express');
 var serveIndex = require('serve-index');
 
@@ -22,4 +25,29 @@ var options = {
 
 //https server
 var https_server = https.createServer(options, app);
+
+//bind socket.io with https_server
+var io = socketIo.listen(https_server);
+
+//connection
+io.sockets.on('connection', (socket)=>{
+
+	socket.on('join', (room)=> {
+		socket.join(room);
+		var myRoom = io.sockets.adapter.rooms[room];
+		var users = Object.keys(myRoom.sockets).length;
+	 	socket.broadcast.emit('joined', room, socket.id);//除自己，全部站点	
+	});
+
+	socket.on('leave', (room)=> {
+		var myRoom = io.sockets.adapter.rooms[room];
+		var users = Object.keys(myRoom.sockets).length;
+		//users - 1;
+
+
+		socket.leave(room);
+	 	socket.emit('leaved', room, socket.id);		
+	});
+});
+
 https_server.listen(443, '0.0.0.0');
