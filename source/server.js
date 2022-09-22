442100af1a91af699444b6b5c6c638c0f91adf12
shawn·black <longshanzhiyu_2018@163.com>
2022-09-22 14:49:31 +0800

    no message

--- source/server.js
+++ source/server.js
@@ -27,27 +27,27 @@ var options = {
 var https_server = https.createServer(options, app);
 
 //bind socket.io with https_server
-var io = socketIo(https_server, {});
+// var io = socketIo(https_server, {});
 
 //connection
-io.sockets.on('connection', (socket)=>{
-    // console.log('connected: ' + socket )
-	socket.on('join', (room)=> {
-		socket.join(room);
-		var myRoom = io.sockets.adapter.rooms[room];
-		var users = Object.keys(myRoom.sockets).length;
-	 	socket.broadcast.emit('joined', room, socket.id);//除自己，全部站点	
-	});
+// io.sockets.on('connection', (socket)=>{
+//     // console.log('connected: ' + socket )
+// 	socket.on('join', (room)=> {
+// 		socket.join(room);
+// 		var myRoom = io.sockets.adapter.rooms[room];
+// 		var users = Object.keys(myRoom.sockets).length;
+// 	 	socket.broadcast.emit('joined', room, socket.id);//除自己，全部站点	
+// 	});
 
-	socket.on('leave', (room)=> {
-		var myRoom = io.sockets.adapter.rooms[room];
-		var users = Object.keys(myRoom.sockets).length;
-		//users - 1;
+// 	socket.on('leave', (room)=> {
+// 		var myRoom = io.sockets.adapter.rooms[room];
+// 		var users = Object.keys(myRoom.sockets).length;
+// 		//users - 1;
 
 
-		socket.leave(room);
-	 	socket.emit('leaved', room, socket.id);		
-	});
-});
+// 		socket.leave(room);
+// 	 	socket.emit('leaved', room, socket.id);		
+// 	});
+// });
 
 https_server.listen(443, '0.0.0.0');
