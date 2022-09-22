91459248444eceb6f2e0e25cf9eb4b5ed13f0e77
shawn·black <longshanzhiyu_2018@163.com>
2022-09-22 15:02:22 +0800

    重新创建文件

--- README.md
+++
@@ -1,8 +0,0 @@
-# webrtcdemo
-
-2022.09.18
-本demo 第一步
-创建https 服务器
-可以利用acme创建 https证书。但没有操作成功 原因未知。可参考https://www.cxymm.net/article/weixin_30569001/96354302
-
-所以，我才用了本地https证书的方式。需要用safari浏览器才可访问，chrome无法打开。无效的证书。参考链接：https://developer.aliyun.com/article/762524
---
+++ src/server.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+
+//socket.io
+var socketIo = require('socket.io');
+
+var express = require('express');
+var serveIndex = require('serve-index');
+
+var app = express();
+app.use(express.static('../public'));
+app.use(serveIndex('../public'));
+
+//http server
+var http_server = http.createServer(app);
+http_server.listen(80, '0.0.0.0');
+
+var options = {
+    key:    fs.readFileSync('../resource/cert/fbird.xyz.key'),
+    cert:   fs.readFileSync('../resource/cert/fbird.xyz.crt')
+}
+
+//https server
+var https_server = https.createServer(options, app);
+https_server.listen(443, '0.0.0.0');
