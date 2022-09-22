c824fac3536bd19a21fa2cd259f321ce75a22242
shawn·black <longshanzhiyu_2018@163.com>
2022-09-22 10:58:00 +0800

    测试

--- source/server.js
+++ source/server.js
@@ -27,7 +27,7 @@ var options = {
 var https_server = https.createServer(options, app);
 
 //bind socket.io with https_server
-var io = socketIo.attach(https_server);
+var io = socketIo.createServer(https_server);
 
 //connection
 // io.sockets.on('connection', (socket)=>{
