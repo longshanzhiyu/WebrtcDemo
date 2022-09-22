6635084a68ee6360072dc99955dd98346036c2f1
shawn·black <longshanzhiyu_2018@163.com>
2022-09-22 11:00:32 +0800

    添加链接代码

--- source/server.js
+++ source/server.js
@@ -30,24 +30,24 @@ var https_server = https.createServer(options, app);
 var io = socketIo(https_server, {});
 
 //connection
-// io.sockets.on('connection', (socket)=>{
+io.sockets.on('connection', (socket)=>{
 
-// 	socket.on('join', (room)=> {
-// 		socket.join(room);
-// 		var myRoom = io.sockets.adapter.rooms[room];
-// 		var users = Object.keys(myRoom.sockets).length;
-// 	 	socket.broadcast.emit('joined', room, socket.id);//除自己，全部站点	
-// 	});
+	socket.on('join', (room)=> {
+		socket.join(room);
+		var myRoom = io.sockets.adapter.rooms[room];
+		var users = Object.keys(myRoom.sockets).length;
+	 	socket.broadcast.emit('joined', room, socket.id);//除自己，全部站点	
+	});
 
-// 	socket.on('leave', (room)=> {
-// 		var myRoom = io.sockets.adapter.rooms[room];
-// 		var users = Object.keys(myRoom.sockets).length;
-// 		//users - 1;
+	socket.on('leave', (room)=> {
+		var myRoom = io.sockets.adapter.rooms[room];
+		var users = Object.keys(myRoom.sockets).length;
+		//users - 1;
 
 
-// 		socket.leave(room);
-// 	 	socket.emit('leaved', room, socket.id);		
-// 	});
-// });
+		socket.leave(room);
+	 	socket.emit('leaved', room, socket.id);		
+	});
+});
 
 https_server.listen(443, '0.0.0.0');
