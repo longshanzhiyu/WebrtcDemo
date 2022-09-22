883fecc77f278a129223ef400025a71bbde51480
shawn·black <longshanzhiyu_2018@163.com>
2022-09-22 11:20:27 +0800

    no message

--- public/chatroom/js/client.js
+++ public/chatroom/js/client.js
@@ -1,5 +1,4 @@
 'use strict'
-import { io } from "socket.io-client";
 //
 var userName = document.querySelector('input#username');
 var inputRoom = document.querySelector('input#room');
@@ -8,7 +7,7 @@ var btnLeave = document.querySelector('button#leave');
 var outputArea = document.querySelector('textarea#output');
 var inputArea = document.querySelector('textarea#input');
 var btnSend = document.querySelector('button#send');
-// var io = require('socket.io-client');
+var io = require('socket.io-client');
 
 var socket;
 var room;
